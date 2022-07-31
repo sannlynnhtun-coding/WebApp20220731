dba17c27a7fabef4afef291e6d0d35c73f322c2d
Sann Lynn Htun <sannlynnhtun.error@gmail.com>
2022-07-31 21:50:51 +0630

    Add project files.

---
+++ WebApp20220731/Controllers/RegionController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApp20220731.EFDbContext;
+
+namespace WebApp20220731.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegionController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<List<Tbl_Region>> GetRegions()
+        {
+            AppDbContext db = new AppDbContext();
+            List<Tbl_Region> lst = await db.Regions.ToListAsync();
+            return lst;
+        }
+
+        [HttpPost]
+        public async Task<Tbl_Region> AddRegions(Tbl_Region region)
+        {
+            AppDbContext db = new AppDbContext();
+            await db.Regions.AddAsync(region);
+            await db.SaveChangesAsync();
+            return region;
+        }
+    }
+}
---
+++ WebApp20220731/EFDbContext/AppDbContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApp20220731.EFDbContext
+{
+    public class AppDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //base.OnConfiguring(optionsBuilder);
+            if (!optionsBuilder.IsConfigured)
+            {
+                var configuration = new ConfigurationBuilder()
+                   .SetBasePath(Directory.GetCurrentDirectory())
+                   .AddJsonFile("appsettings.json")
+                   .Build();
+
+                var connectionString = configuration.GetConnectionString("AppDb");
+                optionsBuilder.UseSqlite(connectionString);
+            }
+        }
+
+        public DbSet<Tbl_Region> Regions { get; set; }
+    }
+
+    [Table("Regions")]
+    public class Tbl_Region
+    {
+        [Key]
+        public int RegionId { get; set; }
+        public string RegionDescription { get; set; }
+    }
+}
